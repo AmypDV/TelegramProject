d5bbcbb275fbfa070ef0c634964448e3344c6935
AmypDV <amypdv@yandex.ru>
2024-06-30 18:57:56 +0300

    добавил клавиатуру в хэндлер_старт

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -2,6 +2,8 @@ from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from aiogram import Router
 
+from keyboards.keyboard_utils import keyboard_animal
+
 from lexicon.lexicon import LEXICON_RU
 import logging
 
@@ -9,12 +11,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 router_command = Router()
+
 # Этот хэндлер будет срабатывать на команду "/start"
 @router_command.message(CommandStart())
 async def process_start_command(message: Message):
     logger.info(f'command start. Member {message.from_user.id}')
-    print(LEXICON_RU().start)
-    await message.answer(text=LEXICON_RU().start)
+    await message.answer(text=LEXICON_RU().start,
+                         reply_markup=keyboard_animal)
+
 
 
 
